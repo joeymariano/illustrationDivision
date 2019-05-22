21c295787271828edde94595e4d1f2845b34055c
joeymariano <joey.michalina.mariano@gmail.com>
2019-05-22 19:33:46 -0400

    arrow fades and deletes

--- scripts/site.js
+++ scripts/site.js
@@ -1249,5 +1249,19 @@ function scrollDown(el_name){
 
 function deleteArrow(){
   let element = document.querySelector('.downArrowContainer')
-  element.parentNode.removeChild(element)
+  fadeAndDelete(element)
+}
+
+function fadeAndDelete(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+            element.parentNode.removeChild('#newDownArrow')
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
 }
